f051e3b2d5a6eb9ad4197b65e3abc444c32739ec
richardangga <richard.angga51@gmail.com>
2020-01-27 03:26:35 -0500

    update commit

--- models/models.py
+++ models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import tools
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 import time
 import logging
 from odoo.tools.translate import _
@@ -26,14 +26,14 @@ PR_LINE_STATES =[('draft','Draft'),
     ('reject','Rejected')]
 
 class ProductRequestFormPr(models.Model):
-    # _name = "vit.product.request"
-    _inherit = "vit.product.request"
+    _name = "vit.product.request"
+    _inherit = ['vit.product.request','mail.thread']
 
     description = fields.Text(string="Description")
-    requesto_id = fields.Many2one(comodel_name='res.users', string='Requester', required=True,default=lambda self: self.env.uid)
-    reviewed_by = fields.Many2one(comodel_name='res.users', string='Reviewed by',default=lambda self: self.env.uid)
-    budgeted_by = fields.Many2one(comodel_name='res.users', string='Budget Confirmed',default=lambda self: self.env.uid)
-    approved_by = fields.Many2one(comodel_name='res.users', string='Approved by',default=lambda self: self.env.uid)
+    requesto_id = fields.Many2one(comodel_name='res.users', string='Requester',required=True,default=lambda self: self.env.uid)
+    reviewed_by = fields.Many2one(comodel_name='hr.employee', states={'draft':[('readonly',True)],'open':[('readonly',True)],'onprogress':[('readonly',False),('required',True)],'reviewed':[('readonly',True)],'budgetconfirm':[('readonly',True)],'approved':[('readonly',True)],'done':[('readonly',True)],'reject':[('readonly',True)]},string='Reviewed by')
+    budgeted_by = fields.Many2one(comodel_name='hr.employee', states={'draft':[('readonly',True)],'open':[('readonly',True)],'onprogress':[('readonly',True)],'reviewed':[('readonly',False),('required',True)],'budgetconfirm':[('readonly',True)],'approved':[('readonly',True)],'done':[('readonly',True)],'reject':[('readonly',True)]},string='Budget Confirmed')
+    approved_by = fields.Many2one(comodel_name='hr.employee', states={'draft':[('readonly',True)],'open':[('readonly',True)],'onprogress':[('readonly',True)],'reviewed':[('readonly',True)],'budgetconfirm':[('readonly',False),('required',True)],'approved':[('readonly',True)],'done':[('readonly',True)],'reject':[('readonly',True)]},string='Approved by')
     state = fields.Selection(PR_STATES,'Status',readonly=True,required=True, default='draft',track_visibility='onchange')
     analytic_account = fields.Many2one(comodel_name='account.analytic.account', string="Analytic Account", related="department_id.analytic_account_id")
     category_id = fields.Many2one(comodel_name='product.category', string='Product Category',
@@ -42,96 +42,93 @@ class ProductRequestFormPr(models.Model):
         states={'draft':[('readonly',False)]},track_visibility='onchange'
         )
     # product_request_aproval_level_ids = fields.One2many(string='Approval Level Line',ondelete="cascade", copy= True,)
-    @api.model
-    def find_notif_users(self):
-        group_obj = self.requesto_id
-        group_ids = group_obj.sudo().search([
-            # ('category_id', '=', 'Sales'),
-            ('name', '=', 'Manager')])
-        return group_ids
-
-    @api.multi
-    def action_draft(self):
-        #set to "draft" state
-        body = _("PR set to draft")
-        self.send_followers()
-        self.update_line_state(PR_STATES[0][0])
-        return self.write({'state':PR_STATES[0][0]})
-
-    @api.multi
-    def action_confirm(self):
-        #set to "open" approved state
-        body = _("PR confirmed")
-        group_obj = self.requesto_id
-        self.send_followers(group_obj)
-        self.send_to_channel(group_obj)
-        self.update_line_state(PR_STATES[1][0])
-        return self.write({'state':PR_STATES[1][0]})
-
-    @api.multi
-    def action_onprogress(self):
-        #set to "onprogress" state
-        body = _("PR on progress")
-        self.send_followers(body)
-        self.update_line_state(PR_STATES[2][0])
-        return self.write({'state':PR_STATES[2][0]})
-
-    @api.multi
-    def action_reviewed(self):
-        body = _("PR Reviewed")
-        self.send_followers(body)
-        self.send_to_channel(body)
-        self.update_line_state(PR_STATES[3][0])
-        return self.write({'state':PR_STATES[3][0]})
-
-    @api.multi
-    def action_budgetconfirm(self):
-        body = _("Budget confirmed")
-        self.send_followers(body)
-        self.send_to_channel(body)
-        self.update_line_state(PR_STATES[4][0])
-        return self.write({'state':PR_STATES[4][0]})
-
-    @api.multi
-    def action_approved(self):
-        body = _("PR Approved")
-        self.send_followers(body)
-        self.send_to_channel(body)
-        self.update_line_state(PR_STATES[5][0])
-        return self.write({'state':PR_STATES[5][0]})
-
-    @api.multi
-    def action_done(self):
-        #set to "done" state
-        body = _("PR done")
-        self.send_followers()
-        self.update_line_state(PR_STATES[6][0])
-        return self.write({'state':PR_STATES[6][0]})
-
-    @api.multi
-    def action_reject(self):
-        #set to "reject" state
-        body = _("PR reject")
-        self.send_followers()
-        self.update_line_state(PR_STATES[7][0])
-        return self.write({'state':PR_STATES[7][0]})
     
+    @api.model
+    def find_notif_users(self,vals):
+        requesto_ids = []
+        if requesto_ids:
+                vals['message_follower_ids'] = [(0,0, {
+                    'res_model':'vit.product.request',
+                    'requesto_id':self.env['requesto_id']})]
+        
     @api.multi
     def send_followers(self, body):
         # to inbox followers and write notes
-        followers = [x.partner_id.id for x in
-            self.message_follower_ids]
+        # followers = [x.requesto_id.id for x in
+        #     self.message_follower_ids]
         self.message_post(body=body,
             type="notification", subtype="mt_comment",
-            partner_ids=followers,)
+            requesto_ids=self.requesto_id)
         return
     
     def send_to_channel(self, body):
         ch_obj = self.env['mail.channel']
         ch = ch_obj.sudo().search([('name','ilike',
-            'via, Mitchell Admin')])
-        body = body + " <a href='#id=%s&view_type=form&model=sale.order&menu_id=138'>%s</a>" % (self.id, self.name)
+            'via, Mitchell Admin')])    
+        body = _("complete")
         ch.message_post( attachment_ids=[], body=body,
             content_subtype='html', message_type='comment',
-            partner_ids=[], subtype='mail.mt_comment')
+            requesto_ids=[], subtype='mail.mt_comment')
         return True
+
+    
+    @api.multi
+    def action_draft(self):
+        #set to "draft" state
+        body = _("PR set to draft")
+        # self.send_followers(body)
+        self.update_line_state(PR_STATES[0][0])
+        return self.write({'state':PR_STATES[0][0]})
+
+    @api.multi
+    def action_confirm(self,body):
+        #set to "open" approved state
+        # body = _("PR confirmed")
+        self.send_followers(body)
+        self.send_to_channel(body)
+        self.update_line_state(PR_STATES[1][0])
+        return self.write({'state':PR_STATES[1][0]})
+
+    @api.multi
+    def action_confirm_request(self):
+        #set to "open" approved state
+        body = _("PR confirmed")
+        # self.send_to_channel(body)
+        self.update_line_state(PR_STATES[2][0])
+        return self.write({'state':PR_STATES[2][0]})
+
+    @api.multi
+    def action_onprogress(self):
+        #set to "onprogress" state
+        body = _("PR on progress")
+        # self.send_followers(body)
+        self.update_line_state(PR_STATES[3][0])
+        return self.write({'state':PR_STATES[3][0]})
+
+    @api.multi
+    def action_reviewed(self):
+        body = _("PR Reviewed")
+        # self.send_followers(body)
+        # self.send_to_channel(body)
+        self.update_line_state(PR_STATES[4][0])
+        return self.write({'state':PR_STATES[4][0]})
+
+    @api.multi
+    def action_budgetconfirm(self):
+        body = _("Budget confirmed")
+        # self.send_followers(body)
+        # self.send_to_channel(body)
+        self.update_line_state(PR_STATES[5][0])
+        return self.write({'state':PR_STATES[5][0]})
+
+    @api.multi
+    def action_approved(self):
+        body = _("PR Approved")
+        self.update_line_state(PR_STATES[6][0])
+        return self.write({'state':PR_STATES[6][0]})
+
+    @api.multi
+    def action_reject(self):
+        body = _("PR reject")
+        self.update_line_state(PR_STATES[7][0])
+        return self.write({'state':PR_STATES[7][0]})
